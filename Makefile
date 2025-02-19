21fbf32c286bd608bf6ca5a6adf1f54c9ae9bf52
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-02-19 10:54:00 -0800

    add: test for basic function + benchmark
    NOTE: benchmark is currently segfaulting; I'll investigate later.

---
+++ Makefile
@@ -0,0 +1,37 @@
+CXXFLAGS_WARN 			:= -Wall -Werror -Wpedantic
+CXXFLAGS_OPTIM 			:= -O3
+CXXFLAGS_INCLUDE_PATHS 	:= -Iinclude
+CXXFLAGS_VERSION		:= -std=c++23
+CXXFLAGS_SAN 			:= -fsanitize=address
+CXXFLAGS := $(CXXFLAGS_WARN) $(CXXFLAGS_OPTIM) $(CXXFLAGS_INCLUDE_PATHS)\
+			$(CXXFLAGS_VERSION) $(CXXFLAGS_SAN)
+CXX := /usr/bin/clang++-18 $(CXXFLAGS)
+LOG_LIB := build/SpektralLogger.so
+
+all: build/LogDemo tests
+tests: build/perfTest
+
+check:
+	cppcheck -Iinclude/ --enable=all --suppress=missingIncludeSystem \
+		--suppress=checkersReport src tests
+
+build/LogDemo: $(LOG_LIB) tests/demo.cpp
+	$(CXX) $^ -o $@
+
+build/perfTest: $(LOG_LIB) tests/Perf.cpp
+	$(CXX) $^ -o $@ -lbenchmark
+
+$(LOG_LIB): build/Logger.o build/LogEvent.o
+	$(CXX) -shared -fPIC $^ -o $@
+
+build/Logger.o: src/Logger.cpp
+	$(CXX) -c -fPIC $^ -o $@
+
+build/LogEvent.o: src/LogEvent.cpp
+	$(CXX) -c -fPIC $^ -o $@
+
+clean:
+	rm -rf build/*
+
+.PHONY: clean check
+
---
+++ tests/demo.cpp
@@ -0,0 +1,35 @@
+#include "Logger.hpp"
+#include "Sources.hpp"
+#include <fstream>
+#include <print>
+
+class Source_int : public Spektral::Log::ISource {
+  int val;
+
+public:
+  operator std::string() { return std::to_string(val); }
+  Source_int(int v) { val = v; }
+  static std::unique_ptr<Source_int> Make(int v) {
+    return std::make_unique<Source_int>(v);
+  }
+};
+
+class Message_int : public Spektral::Log::IMessage {
+  int val;
+
+public:
+  operator std::string() { return std::to_string(val); }
+  Message_int(int v) { val = v; }
+  static std::unique_ptr<Message_int> Make(int v) {
+    return std::make_unique<Message_int>(v);
+  }
+};
+
+auto main() -> int {
+  std::ofstream l{"output_logs/demo.log"};
+  Spektral::Log::Logger i(l);
+  for (int ii = 0; ii < 50000; ++ii)
+    i.insert_INFO({Spektral::Log::INFO,
+                   Spektral::Log::Source<std::string>::Make("main"),
+                   Message_int::Make(ii)});
+}
